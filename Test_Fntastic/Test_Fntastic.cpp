6a4d78345c12d1b509ae630c805653c3230e6d0b
sanchess <Sanchess2015@yandex.ru>
2021-10-24 09:24:31 +0300

    no message

---
+++ Test_Fntastic/Test_Fntastic.cpp
@@ -0,0 +1,32 @@
+﻿// Test_Fntastic.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int len = 0;
+	string s;
+	cout << "Введите строку: ";
+	cin >> s;
+	while (len != s.length()) {
+		for (int i = len; i < s.length(); i++) {
+				if (s[len] == s[i + 1]) {
+					s[len] = ')';
+					cout << s[len];
+					break;
+				}
+				else {
+					s[len] = '(';
+					cout << s[len];
+					break;
+				}
+		}
+		len++;
+	}
+	system("pause");
+	return 0;
+}
+
